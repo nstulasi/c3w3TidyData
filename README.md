5eeffeb4abf03d94ad0899db30f642157e0f017a
tulasi.neppali@gmail.com <saneppal@microsoft.com>
2014-05-25 09:31:54 -0700

    Final edits

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ c3w3TidyData
 ============
 
 Course 3 (Getting and Cleaning data) Week 3 peer assessment homework
+
+The following were steps in creating the tidy data set
+
+1. Download and unzip the data set
+2. Update the names of the columns to meaningful terms
+3 Extract features of "mean" and "std". These can be found by searching for features that end in those terms
+4. Merge the test and training data sets
+5. Perform calculations on the merged set to extract for every activity and subject the average variable.
--- run_analysis.R
+++ run_analysis.R
@@ -45,8 +45,4 @@ tidy_data<-rbind(x_train,x_test)
 # Average of each variable by subject and activity
 tidy_data<-data.table(tidy_data)
 tidy_data<-tidy_data[, lapply(.SD,mean), by=c("activity","subject")]
-write.table(tidy_data, file="./tidydata.txt", sep="\t", row.names=FALSE)
-#Extracts only the measurements on the mean and standard deviation for each measurement. 
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive activity names. 
-#Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+write.table(tidy_data2, file="./tidydata.txt", sep="\t", row.names=FALSE)
